bde82cc16e94876585244efc0247cc274a331e4a
@mihirwebnys <hiteshhirpara3862@gmail.com>
2024-10-17 22:38:14 -0400

    second commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.engine('hbs', handlebars.engine({extname: '.hbs',
     }
  }));
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const urlencodedParser = express.urlencoded({ extended: false })
 
